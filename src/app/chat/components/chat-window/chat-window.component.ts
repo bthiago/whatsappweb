c9d56475520b182e7d64e9178a79d9da5702b8a4
Kunal Ganglani <kunalganglani@gmail.com>
2018-07-09 01:34:06 +0530

    chat window made dynamic

--- src/app/chat/components/chat-window/chat-window.component.ts
+++ src/app/chat/components/chat-window/chat-window.component.ts
@@ -7,12 +7,77 @@ import { ChatMsg } from '../single-chat/ChatMsg';
   styleUrls: ['./chat-window.component.css']
 })
 export class ChatWindowComponent {
-  msg: ChatMsg = {
+  chatHistory = {
+    sent : true,
+    msgText : 'Hey, How are you???'
+  };
+  msg = {
     'senderID': 1,
     'senderName': 'Ced MacCague',
     'lastMessage': 'brand bricks-and-clicks synergies',
     'timeStamp': '5:25 AM',
-    'senderImage': 'https://robohash.org/quisetnam.jpg?size=50x50&set=set1'
+    'senderImage': 'https://robohash.org/quisetnam.jpg?size=50x50&set=set1',
+    'chatHistory': [{
+      'sent': false,
+      'msgText': 'orchestrate granular partnerships'
+    }, {
+      'sent': true,
+      'msgText': 'repurpose open-source bandwidth'
+    }, {
+      'sent': false,
+      'msgText': 'harness impactful relationships'
+    }, {
+      'sent': true,
+      'msgText': 'matrix enterprise models'
+    }, {
+      'sent': false,
+      'msgText': 'enable value-added e-services'
+    }, {
+      'sent': true,
+      'msgText': 'extend killer e-business'
+    }, {
+      'sent': true,
+      'msgText': 'extend transparent infrastructures'
+    }, {
+      'sent': true,
+      'msgText': 'evolve wireless convergence'
+    }, {
+      'sent': false,
+      'msgText': 'optimize plug-and-play platforms'
+    }, {
+      'sent': false,
+      'msgText': 'aggregate sexy portals'
+    }, {
+      'sent': false,
+      'msgText': 'deliver compelling applications'
+    }, {
+      'sent': false,
+      'msgText': 'optimize 24/7 eyeballs'
+    }, {
+      'sent': false,
+      'msgText': 'extend interactive metrics'
+    }, {
+      'sent': true,
+      'msgText': 'morph turn-key networks'
+    }, {
+      'sent': true,
+      'msgText': 'evolve bleeding-edge e-tailers'
+    }, {
+      'sent': true,
+      'msgText': 'iterate cross-platform platforms'
+    }, {
+      'sent': false,
+      'msgText': 'generate 24/7 platforms'
+    }, {
+      'sent': true,
+      'msgText': 'exploit 24/365 metrics'
+    }, {
+      'sent': true,
+      'msgText': 'revolutionize frictionless web-readiness'
+    }, {
+      'sent': false,
+      'msgText': 'engage e-business synergies'
+    }]
   };
   @Output()
   showOptions: EventEmitter<any> = new EventEmitter<any>();
